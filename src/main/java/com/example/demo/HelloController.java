e5a7181a7625d64fd1ed818cdfa9ac4a8e0bb228
Adarsh Pandey <pandeyssj@gmail.com>
2026-01-07 03:13:27 +0530

    Add greet API with request parameter

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -14,5 +18,13 @@ public class HelloController {
     @GetMapping("/health")
     public String health() {
         return "OK";
+    }
+
+    @GetMapping("/greet")
+    public Map<String,String> greet(@RequestParam String name)
+    {
+        Map<String, String > r=new HashMap<>();
+        r.put("message",name);
+        return r;
     }
 }
